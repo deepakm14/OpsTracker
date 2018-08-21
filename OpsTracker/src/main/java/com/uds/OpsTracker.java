f312f08e3516b1e86e15af92cf579009871e5ea0
udsin\Deepak.M <deepakm1051@gmail.com>
2018-08-21 10:55:37 +0530

    adding tomcat dependency in pom.xml

--- OpsTracker/src/main/java/com/uds/OpsTracker.java
+++ OpsTracker/src/main/java/com/uds/OpsTracker.java
@@ -1,18 +1,20 @@
 package com.uds;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @ComponentScan(basePackages= {"com.uds.*"})
-public class OpsTracker  {
-	
-	@Autowired
-	private Environment envVariable;
+public class OpsTracker extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(OpsTracker.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(OpsTracker.class, args);
 	}
